fab76d6499993c48a4eb8bbf8fe5a6bf0982001b
Ben <24gluck_benjamin@dublinstudents.net>
2023-03-25 15:26:33 -0400

    login

--- src/models.py
+++ src/models.py
@@ -1,13 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, db.ForeignKey("username"))
-    email = db.Column(db.String, db.ForeignKey("email"))
-    password = db.Column(db.String, db.ForeignKey("password"))
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return f"UserP'{self.id}', '{self.username}', '{self.email}', '{self.password}'"
+        return '<User({username!r})>'.format(username=self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
