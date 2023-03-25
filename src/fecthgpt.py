63e2ed8960e137ca139c7b7965b0ec8839cd5185
Koodsu <97322081+Koodsu@users.noreply.github.com>
2023-03-25 13:40:00 -0400

    Fixed Ben Error

--- src/fecthgpt.py
+++ src/fecthgpt.py
@@ -9,7 +9,7 @@ model_engine = "gpt-3.5-turbo"
 max_tokens = 256
 
 def getGPTResponse(role, text, data):
-    prompt = f"{data["name"]} is a {data["age"]} year old who lives in {data["location"]}. He is a {data["occupation"]} and likes to do {data["hobbies"]}Give me some trends about climate change where he lives and a list of things (specific to where he lives) he can do to help the climate where he lives "
+    prompt = f"{data['name']} is a {data['age']} year old who lives in {data['location']}. He is a {data['occupation']} and likes to do {data['hobbies']}Give me some trends about climate change where he lives and a list of things (specific to where he lives) he can do to help the climate where he lives "
     message = [{"role" : role, "content" : text}]
 
     completion = openai.ChatCompletion.create(
