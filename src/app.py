a24f1323c64d1b5991035db43a2ef4fcc3fc4803
Ben <24gluck_benjamin@dublinstudents.net>
2023-03-25 10:42:21 -0400

    add blueprints

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from forms import forms_blueprint
+from login import login_blueprint
 
 app = Flask(__name__)
+
+app.register_blueprint(forms_blueprint)
+app.register_blueprint(login_blueprint)
 
 @app.route('/')
 def hello_world():
---
+++ src/forms.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+forms_blueprint = Blueprint('forms', __name__, template_folder='templates')
