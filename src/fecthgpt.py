5e860cad3450a2b08817e393aefec0bef1659a0a
Koodsu <97322081+Koodsu@users.noreply.github.com>
2023-03-25 11:03:47 -0400

    Update fecthgpt.py

--- src/fecthgpt.py
+++ src/fecthgpt.py
@@ -1,1 +1,17 @@
+import openai
 
+openai.api_key = "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"
+model_engine = "gpt-3.5-turbo"
+prompt = "Write an essay about climate change"
+
+max_tokens = 256
+
+messages = [{"role": "system", "content": "Write an essay about climate change"}]
+
+completion = openai.ChatCompletion.create(
+    model=model_engine,
+    messages = messages
+
+)
+
+print(completion.choices[0]['message']["content"])
