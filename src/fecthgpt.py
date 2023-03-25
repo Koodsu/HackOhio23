154e690d33707286fb0049aabc7a309305d6bd57
Koodsu <97322081+Koodsu@users.noreply.github.com>
2023-03-25 15:05:08 -0400

    Langchain

--- src/fecthgpt.py
+++ src/fecthgpt.py
@@ -3,15 +3,40 @@ from dotenv import load_dotenv
 import os
 import requests
 import json
+from langchain.chat_models import ChatOpenAI
+from langchain import LLMChain
+from langchain import PromptTemplate
 load_dotenv()
+
+
+template = """Question: {question}
+
+Answer: """
+prompt = PromptTemplate(
+        template=template,
+    input_variables=['question']
+)
 
 
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 
 max_tokens = 256
 
-def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=None):
-    headers = {
+def generate_chat_completion(question, model="gpt-4", temperature=1, max_tokens=None):
+    
+
+    gpt4 = ChatOpenAI(model_name="gpt-4")
+    
+    llm_chain = LLMChain(
+        prompt=prompt,
+        llm=gpt4
+    )
+
+    print(llm_chain.run(question))
+
+
+
+    '''headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}",
     }
@@ -30,6 +55,7 @@ def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=
     if response.status_code == 200:
         return response.json()["choices"][0]["message"]["content"]
     else:
-        raise Exception(f"Error {response.status_code}: {response.text}")
+        raise Exception(f"Error {response.status_code}: {response.text}")'''
 
 
+generate_chat_completion("Who won the superbowl in 2010")
